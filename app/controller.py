d7e166aff0a5bdbc13d8154338f1dc9b13457fe8
ange47rm <ange47rm@hotmail.com>
2020-12-04 20:00:29 +0000

    Nothing is working, great

--- app/controller.py
+++ app/controller.py
@@ -1,14 +1,30 @@
 from flask import render_template, request
 from app import app
-from app.models.game import Game, choices
+from app.models.game import Game
 from app.models.player import Player
 
-@app.route ('/')
-def index():
+@app.route ('/welcome')
+def welcome():
+    return render_template ('welcome.html')
+
+@app.route ('/play', methods=['GET', 'POST'])
+def play():
+    player_name = request.form['name']
+    player_choice = request.form['choice']
+    player = Player (player_name, player_choice)
+    new_game = Game ()
+    new_game.play (player)
+    print (request.form)
     return render_template ('index.html')
 
-@app.route ('/rock/scissors')
-def play_game():
-    game = Game ()
-    game.play()
-    return f'The'
+
+
+
+
+
+
+# @app.route ('/player_1/player_2')
+# def play_game():
+#     new_game = Game ()
+#     new_game.play (player_1, player_2)
+#     return f'The winner of the game is {new_game.winner}'
--- app/models/game.py
+++ app/models/game.py
@@ -1,5 +1,11 @@
+import random
 
-from player import Player
+if __name__ == "__main__":
+    from player import Player
+
+else:
+    from app.models.player import Player
+
 
 choices = ['rock', 'paper', 'scissors']
 
@@ -8,7 +14,9 @@ class Game ():
         self.name = "Rock Paper Scissors"
         self.winner = ''
 
-    def play (self, player_1, player_2):
+    def play (self, player_1):
+
+        player_2 = Player('Computer', random.choice(choices))
 
         if player_1.choice == 'rock' and player_2.choice == 'scissors':
             self.winner = player_1.name
@@ -26,16 +34,13 @@ class Game ():
             self.winner = None
             return self.winner
 
-        elif player_1.choice or player_2.choice not in choices:
-            self.winner = None
-            print (f"One or both player's choices not valid. Choose between 'rock', 'paper' or 'scissors'.")
-
         else:
             self.winner = player_2.name
             return self.winner
 
+#  TESTS SETUP (tests passed)
+
 player_1 = Player ('Angelo', "Rock")
-player_2 = Player ('Richard', "Papser")
 new_game = Game ()
-new_game.play (player_1, player_2)
+new_game.play (player_1)
 print (new_game.winner)
